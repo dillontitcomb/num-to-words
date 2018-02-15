71aab04c2ec696e93e05e60b859e3d7f6d58f57b
Guest User <Guest@epicodus-32.local>
2018-02-15 09:05:01 -0800

    Adds basic directory and file structure for reusable c sharp template.

---
+++ Template.Tests/ModelTests/TemplateTest.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Template;
+using System.Collections.Generic;
+using Template.Models;
+
+namespace Template.Tests
+{
+}
---
+++ Template/Models/Template.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Models
+{
+  public class TemplateClass
+  {
+  }
+}
